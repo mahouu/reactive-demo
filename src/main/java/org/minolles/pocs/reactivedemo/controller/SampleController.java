4b965251263ac3fc7fbbb2df0ee5de0e1aec02bc
Mauricio Miralles <mahouu@gmail.com>
2021-06-27 11:27:18 +0100

    added a controller with dummy examples

--- src/main/java/org/minolles/pocs/reactivedemo/controller/SampleController.java
+++ src/main/java/org/minolles/pocs/reactivedemo/controller/SampleController.java
@@ -1,2 +1,27 @@
-package org.minolles.pocs.reactivedemo.controller;public class SampleController {
+package org.minolles.pocs.reactivedemo.controller;
+
+import java.time.Duration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class SampleController {
+    @GetMapping("/sample")
+    Mono<String> helloWorld(){
+        return Mono.just("Hello world");
+    }
+
+    @GetMapping("/multiple")
+    Flux<Integer> multiple(){
+        return Flux.just(1,2,3,4,5);
+    }
+
+    @GetMapping("/withDelay")
+    Flux<Integer> withDelay(){
+        return Flux.just(1,2,3,4,5,6,7,8,9).delayElements(Duration.ofSeconds(1L)).log();
+    }
 }
